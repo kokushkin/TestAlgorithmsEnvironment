b8dbe972fd2964097d6533ba3cf01e16fca50b9e
Nickolay Kulikov <fipsiksi@yandex.ru>
2020-12-27 22:32:22 +0200

    add first real test

--- src/algoritms.test.js
+++ src/algoritms.test.js
@@ -1,3 +1,23 @@
+import _ from "lodash";
+import { algoritms } from "./algoritms.js";
+
 test("adds 1 + 2 to equal 3", () => {
   expect(true).toBe(true);
+});
+
+test("correctness test", () => {
+  const arr1 = [
+    { key: 2, name: "Igor" },
+    { key: 1, name: "Vasia" }
+  ];
+  const arr2 = [
+    { key: 3, age: 40 },
+    { key: 1, age: 25 }
+  ];
+
+  expect(algoritms.simpleJoinByTretiak(arr1, arr2)).toEqual([
+    { key: 1, name: "Vasia", age: 25 },
+    { key: 2, name: "Igor" },
+    { key: 3, age: 40 }
+  ]);
 });
