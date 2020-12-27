814acefe14aa77d5ea527bc0456ffadc5157eede
Nickolay Kulikov <fipsiksi@yandex.ru>
2020-12-27 22:48:31 +0200

    add all correctness tests

--- src/algoritms.test.js
+++ src/algoritms.test.js
@@ -5,19 +5,23 @@ test("adds 1 + 2 to equal 3", () => {
   expect(true).toBe(true);
 });
 
-test("correctness test", () => {
-  const arr1 = [
-    { key: 2, name: "Igor" },
-    { key: 1, name: "Vasia" }
-  ];
-  const arr2 = [
-    { key: 3, age: 40 },
-    { key: 1, age: 25 }
-  ];
+const arr1 = [
+  { key: 2, name: "Igor" },
+  { key: 1, name: "Vasia" }
+];
+const arr2 = [
+  { key: 3, age: 40 },
+  { key: 1, age: 25 }
+];
 
-  expect(algoritms.simpleJoinByTretiak(arr1, arr2)).toEqual([
-    { key: 1, name: "Vasia", age: 25 },
-    { key: 2, name: "Igor" },
-    { key: 3, age: 40 }
-  ]);
+const rest = new Set([
+  { key: 1, name: "Vasia", age: 25 },
+  { key: 2, name: "Igor" },
+  { key: 3, age: 40 }
+]);
+
+_.forEach(algoritms, (alg, key) => {
+  test(key, () => {
+    expect(new Set(alg(arr1, arr2))).toEqual(rest);
+  });
 });
